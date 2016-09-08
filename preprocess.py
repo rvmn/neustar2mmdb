ab673ed36555caace1f863fa5902f4ba2edda0f7
rvmn <rvmn@users.noreply.github.com>
2016-09-08 12:37:05 +0200

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -15,7 +15,7 @@ import netaddr
 
 
 '''klady@klady:/data/neustar $ head v727.281_24.50_20150320.csv
-"start_ip_int","end_ip_int","continent","country","country_code","country_cf","region","state","state_code","state_cf","city","city_cf","postal_code","area_code","time_zone","latitude","longitude","dma","msa","connection_type","line_speed","ip_routing_type","asn","sld","tld","organization","carrier","anonymizer_status","home","organization_type","naics_code","isic_code","geonames_id","state_ref_id","region_ref_id","city_ref_id","proxy_type","proxy_level","proxy_last_detected","hosting_facility"'''
+"start_ip_int","end_ip_int","country_code","country","state","city","latitude","longitude","time_zone"'''
 
 
 def process_row(row, fields):
